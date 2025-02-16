37504bf2168d61d49d6b333811a742ec5b3eae6a
Mike Niewoehner <Mikeniewoehner850@gmail.com>
2025-02-15 20:52:29 -0500

    Added message board to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import "./Home.css";
+import MessageBoard from "../components/MessageBoard";
 
 function Home() {
   return (
     <div className="home-container">
       <h1 className="page-title">🏡 Welcome to The Long Bomb Cup Web App</h1>
       <p>
-        This is your central hub for our annual golf trip! Here you can view the countdown
-        to our next outing, check the latest scores and challenges, and connect with fellow participants.
-        Nate and Matt are going down. 
+        This is your central hub for our annual golf trip! View the countdown to our next outing,
+        check scores and challenges, and connect with fellow participants.
       </p>
-      <p>
-        Explore the features, join in on the challenges, and enjoy staying updated on everything
-        related to the Long Bomb Cup.
-      </p>
+      {/* Message Board Section */}
+      <MessageBoard />
     </div>
   );
 }
